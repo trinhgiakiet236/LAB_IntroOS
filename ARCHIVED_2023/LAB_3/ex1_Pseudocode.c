74c33a31b4e9490fdaf3354245facccf6f32eb43
trinhgiakiet236 <52400019@student.tdtu.edu.vn>
2025-03-05 10:05:25 +0700

    Update ex1_Pseudocode.c

--- ARCHIVED_2023/LAB_3/ex1_Pseudocode.c
+++ ARCHIVED_2023/LAB_3/ex1_Pseudocode.c
@@ -2,39 +2,44 @@
 // Creator: Tran Trung Tin
 // Exercise
 #include <sys/types.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
-
 #include <sys/wait.h>
 
 int main() {
-  pid_t pid;
+    pid_t pid;
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+ 
+    pid = fork();
+    
+    if (pid < 0) {
   
-  // khai báo biến toàn cục n
-  // lấy giá trị n vào từ lời gọi
-  
-  /* fork a child process */
-  pid = fork();
-  if (pid < 0) {
-    /* error occurred */
-    fprintf(stderr, "Fork Failed");
-    return 1;
-  } else if (pid == 0) {
-    /* child process */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    } else if (pid == 0) {
+         int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        printf("Child: The sum of numbers from 1 to %d is %d\n", n, sum);
+        
+    } else {
+       
+        
+        printf("Parent: The divisors of %d are: ", n);
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+
+   
+        wait(NULL);
+    }
     
-    // vòng lặp tính tổng S
-    // in ra S
-    
-  } else {
-    /* parent process */
-    // tính ước số 
-    // in ra
-    
-    wait(NULL);
-    
-    
-  }
-  return 0;
+    return 0;
 }
