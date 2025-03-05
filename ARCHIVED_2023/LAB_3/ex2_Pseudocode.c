a73e390bbe4a43f14c1a8b31ab1d4bf542e70557
trinhgiakiet236 <52400019@student.tdtu.edu.vn>
2025-03-05 10:06:30 +0700

    Update ex2_Pseudocode.c

--- ARCHIVED_2023/LAB_3/ex2_Pseudocode.c
+++ ARCHIVED_2023/LAB_3/ex2_Pseudocode.c
@@ -2,37 +2,43 @@
 // Creator: Tran Trung Tin
 // Demo using system call fork to create a child process which execute command ls by execlp.
 #include <sys/types.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
-
 #include <sys/wait.h>
 
 int main() {
-  pid_t pid;
-  
-  // Khai báo biến n toàn cục
-  // Nhận n từ lời gọi 
-  
-  /* fork a child process */
-  pid = fork();
-  if (pid < 0) {
-    /* error occurred */
-    fprintf(stderr, "Fork Failed");
-    return 1;
-  } else if (pid == 0) {
-    /* child process */
-    //Vòng lặp tính và in ra dãy Collatz
-    //trong khi mà n > 0
-      // nếu n chẵn thì .....
-      // nếu n lẻ thì ......
-      // in ra n
-  } else {
-    /* parent process */
-    // Đơn giản là chỉ đứng chờ con:
-    wait(NULL);
+    pid_t pid;
+    int n;
     
-  }
-  return 0;
+    // Nhận giá trị n từ người dùng
+    printf("Enter a number: ");
+    scanf("%d", &n);
+  
+    /* Fork a child process */
+    pid = fork();
+  
+    if (pid < 0) {
+        /* Error occurred */
+        fprintf(stderr, "Fork Failed\n");
+        return 1;
+    } else if (pid == 0) {
+        /* Child process: Calculate and print the Collatz sequence */
+        
+        printf("Child: Collatz sequence for %d is: ", n);
+        while (n != 1) {
+            printf("%d ", n);
+            if (n % 2 == 0) {
+                n /= 2; // If n is even, divide by 2
+            } else {
+                n = 3 * n + 1; // If n is odd, multiply by 3 and add 1
+            }
+        }
+        printf("1\n"); // Final 1 of the sequence
+    } else {
+        /* Parent process: Just waits for the child process to finish */
+        wait(NULL);
+        printf("Parent: Child process finished\n");
+    }
+  
+    return 0;
 }
